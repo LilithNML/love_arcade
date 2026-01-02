ee4439834c3ccfaad6d928c9bfeea4e2a530267b
LilithNML <lilithmorningstar468@gmail.com>
2026-01-02 09:57:15 -0600

    Update service-worker.js

--- games/rompecabezas/service-worker.js
+++ games/rompecabezas/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * service-worker.js v2.7.5
+ * service-worker.js v2.8
  * Encargado del cacheo de recursos para funcionamiento Offline (PWA).
  * CORRECCIÓN APLICADA: Ruta de levels.json actualizada a ./public/
  */
 
-const CACHE_NAME = 'puzzle-v2.7.5-Haptics & Visual Assist'; // Versión actualizada para limpiar caché viejo
+const CACHE_NAME = 'puzzle-v2.8-Retro Arcade Synthwave UI'; // Versión actualizada para limpiar caché viejo
 
 // 1. GENERACIÓN DINÁMICA DE RUTAS DE NIVELES
 // Evita escribir manualmente 40 líneas de código para imágenes y thumbnails.
