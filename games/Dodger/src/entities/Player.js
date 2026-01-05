067605f3d6553c2f1e167d01946bcc57d171a1c8
LilithNML <lilithmorningstar468@gmail.com>
2026-01-05 14:33:50 -0600

    Refactor Player class with color and glow properties

--- games/Dodger/src/entities/Player.js
+++ games/Dodger/src/entities/Player.js
@@ -3,15 +3,21 @@ export default class Player {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
         this.size = 20;
-        this.color = '#3b82f6'; // Azul Tailwind
+        this.color = '#3b82f6'; // Default Blue
+        this.glowColor = '#60a5fa';
         
-        // Físicas de la beta
+        // Físicas
         this.x = gameWidth / 2 - this.size / 2;
         this.y = gameHeight - this.size - 50;
         this.speed = 0;
         this.maxSpeed = 8;
         this.acceleration = 0.8;
         this.friction = 0.85;
+    }
+
+    setColor(palette) {
+        this.color = palette.player;
+        this.glowColor = palette.glow;
     }
 
     update(dt, input) {
@@ -38,10 +44,20 @@ export default class Player {
 
     draw(ctx) {
         ctx.save();
-        ctx.shadowBlur = 20;
-        ctx.shadowColor = this.color;
+        
+        // Efecto Neón
+        ctx.shadowBlur = 15;
+        ctx.shadowColor = this.glowColor;
         ctx.fillStyle = this.color;
+        
+        // Dibujar nave
         ctx.fillRect(this.x, this.y, this.size, this.size);
+        
+        // Núcleo blanco (estilo Arcade)
+        ctx.shadowBlur = 0;
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(this.x + 5, this.y + 5, this.size - 10, this.size - 10);
+        
         ctx.restore();
     }
 }
