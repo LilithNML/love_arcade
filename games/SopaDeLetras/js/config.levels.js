4b7d5379458a8289ef41d5b119bfcab7064ac764
LilithNML <lilithmorningstar468@gmail.com>
2026-01-29 02:08:27 -0600

    Create config.levels.js

---
+++ games/SopaDeLetras/js/config.levels.js
@@ -0,0 +1,25 @@
+// config.levels.js
+window.LA_WS_LEVELS = [
+  {
+    id: "la_ws_lvl_01",
+    title: "PLANETAS",
+    gridSize: 10,
+    words: ["TIERRA", "MARTE", "JUPITER", "SATURNO", "VENUS"],
+    rewardCoins: 50
+  },
+  {
+    id: "la_ws_lvl_02",
+    title: "ANIMALES",
+    gridSize: 10,
+    words: ["PERRO", "GATO", "ELEFANTE", "JIRAFA", "TIGRE"],
+    rewardCoins: 60
+  },
+  {
+    id: "la_ws_lvl_03",
+    title: "ARCADE",
+    gridSize: 12,
+    words: ["PACMAN", "TETRIS", "PONG", "GALAGA", "ASTEROIDS"],
+    rewardCoins: 100
+  }
+  // Se pueden añadir >20 niveles aquí siguiendo el mismo formato
+];
