acbf1b4f56ca3448ba122f32a603eb88f34596fc
LilithNML <lilithmorningstar468@gmail.com>
2026-01-31 23:43:22 -0600

    Update service-worker.js

--- games/rompecabezas/service-worker.js
+++ games/rompecabezas/service-worker.js
@@ -4,7 +4,7 @@
  * CORRECCIÓN APLICADA: Ruta de levels.json actualizada a ./public/
  */
 
-const CACHE_NAME = 'puzzle-v3.01'; // Versión actualizada para limpiar caché viejo
+const CACHE_NAME = 'puzzle-v3.02'; // Versión actualizada para limpiar caché viejo
 
 // 1. GENERACIÓN DINÁMICA DE RUTAS DE NIVELES
 // Evita escribir manualmente 40 líneas de código para imágenes y thumbnails.
